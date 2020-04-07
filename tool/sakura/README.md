8804af4261d3747919751ab6e7321cfdf6874434
samwhelp <samwhelp@users.noreply.github.com>
2020-04-07 23:12:55 +0800

    sakura

---
+++ tool/sakura/README.md
@@ -0,0 +1,63 @@
+
+# Tool / sakura
+
+
+## Package
+
+* [sakura](https://packages.ubuntu.com/focal/sakura)
+
+
+## Manpage
+
+* $ man [sakura](http://manpages.ubuntu.com/manpages/focal/man1/sakura.1.html)
+
+
+## Config Dir Path
+
+* [~/.config/sakura](config/sakura)
+
+
+## Config File Path
+
+* [~/.config/sakura/sakura.conf](config/sakura/sakura.conf)
+
+
+## Howto
+
+### install
+
+``` sh
+$ ./install.sh
+```
+
+or
+
+``` sh
+$ make install
+```
+
+
+### package-install
+
+``` sh
+$ ./package-install.sh
+```
+
+or
+
+``` sh
+$ make package-install
+```
+
+
+### config-install
+
+``` sh
+$ ./config-install.sh
+```
+
+or
+
+``` sh
+$ make config-install
+```
---
+++ tool/sakura/config-install.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+################################################################################
+### Head: sakura
+##
+sakura_config_install () {
+
+	echo "mkdir -p $HOME/.config/sakura"
+	mkdir -p "$HOME/.config/sakura"
+	
+
+	echo "cp ./config/sakura/sakura.conf $HOME/.config/sakura/sakura.conf"
+	cp "./config/sakura/sakura.conf" "$HOME/.config/sakura/sakura.conf"
+
+	sakura_put_my_desktop_entry
+
+}
+
+sakura_put_my_desktop_entry () {
+
+	echo "mkdir -p $HOME/.local/share/applications"
+	mkdir -p "$HOME/.local/share/applications"
+
+	echo "cp /usr/share/applications/sakura.desktop $HOME/.local/share/applications/sakura.desktop"
+	cp "/usr/share/applications/sakura.desktop" "$HOME/.local/share/applications/sakura.desktop"
+
+	echo "sed -i 's/^Exec=sakura/Exec=sakura -m/g' $HOME/.local/share/applications/sakura.desktop"
+	sed -i 's/^Exec=sakura/Exec=sakura -m/g' "$HOME/.local/share/applications/sakura.desktop"
+
+}
+##
+### Tail: sakura
+################################################################################
+
+
+################################################################################
+### Head: main
+##
+main_config_install () {
+
+	sakura_config_install
+
+}
+## start
+main_config_install
+
+##
+### Tail: main
+################################################################################
