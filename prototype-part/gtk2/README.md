92397ed4b9a67c901863ab7bbaff13ccc7619976
samwhelp <samwhelp@users.noreply.github.com>
2020-04-08 00:10:14 +0800

    sync

---
+++ prototype-part/gtk2/README.md
@@ -0,0 +1,53 @@
+
+# Part / gtk2
+
+
+## Config Dir Path
+
+* [~/](config/gtk2)
+
+
+## Config File Path
+
+* [~/.gtkrc-2.0](config/gtk2/.gtkrc-2.0)
+
+
+## Howto
+
+### install
+
+``` sh
+$ ./install.sh
+```
+
+or
+
+``` sh
+$ make install
+```
+
+
+### package-install
+
+``` sh
+$ ./package-install.sh
+```
+
+or
+
+``` sh
+$ make package-install
+```
+
+
+### config-install
+
+``` sh
+$ ./config-install.sh
+```
+
+or
+
+``` sh
+$ make config-install
+```
---
+++ prototype-part/gtk2/sync.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+################################################################################
+### Head: config_profile_sync
+##
+gtk2_config_profile_sync () {
+
+	echo "cp './config/gtk2/.gtkrc-2.0' '../../prototype/openbox/config/gtk2/.gtkrc-2.0'"
+	cp './config/gtk2/.gtkrc-2.0' '../../prototype/openbox/config/gtk2/.gtkrc-2.0'
+
+
+	echo "cp './config/gtk2/.gtkrc-2.0' '../../prototype/bspwm/config/gtk2/.gtkrc-2.0'"
+	cp './config/gtk2/.gtkrc-2.0' '../../prototype/bspwm/config/gtk2/.gtkrc-2.0'
+
+
+	echo "cp './config/gtk2/.gtkrc-2.0' '../../prototype/spectrwm/config/gtk2/.gtkrc-2.0'"
+	cp './config/gtk2/.gtkrc-2.0' '../../prototype/spectrwm/config/gtk2/.gtkrc-2.0'
+
+
+	echo "cp './config/gtk2/.gtkrc-2.0' '../../prototype/xfce/config/gtk2/.gtkrc-2.0'"
+	cp './config/gtk2/.gtkrc-2.0' '../../prototype/xfce/config/gtk2/.gtkrc-2.0'
+
+
+	echo "cp './config/gtk2/.gtkrc-2.0' '../../prototype/lxqt-with-openbox/config/gtk2/.gtkrc-2.0'"
+	cp './config/gtk2/.gtkrc-2.0' '../../prototype/lxqt-with-openbox/config/gtk2/.gtkrc-2.0'
+
+
+	echo "cp './config/gtk2/.gtkrc-2.0' '../../prototype/lxqt-with-xfwm4/config/gtk2/.gtkrc-2.0'"
+	cp './config/gtk2/.gtkrc-2.0' '../../prototype/lxqt-with-xfwm4/config/gtk2/.gtkrc-2.0'	
+
+
+	echo "cp './config/gtk2/.gtkrc-2.0' '../../prototype-xsession/xsession-wm-xfwm4/xsession-wm-xfwm4-with-tint2/config/gtk2/.gtkrc-2.0'"
+	cp './config/gtk2/.gtkrc-2.0' '../../prototype-xsession/xsession-wm-xfwm4/xsession-wm-xfwm4-with-tint2/config/gtk2/.gtkrc-2.0'
+
+
+	echo "cp './config/gtk2/.gtkrc-2.0' '../../prototype-xsession/xsession-wm-xfwm4/xsession-wm-xfwm4-with-cairo-dock/config/gtk2/.gtkrc-2.0'"
+	cp './config/gtk2/.gtkrc-2.0' '../../prototype-xsession/xsession-wm-xfwm4/xsession-wm-xfwm4-with-cairo-dock/config/gtk2/.gtkrc-2.0'	
+
+}
+##
+### Tail: config_profile_sync
+################################################################################
+
+
+################################################################################
+### Head: main
+##
+main_sync () {
+
+	gtk2_config_profile_sync
+
+}
+## start
+main_sync
+
+##
+### Tail: main
+################################################################################
