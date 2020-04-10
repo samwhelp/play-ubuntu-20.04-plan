1a80d259edfee9404c8f687779d0a15f56014322
samwhelp <samwhelp@users.noreply.github.com>
2020-04-10 22:22:59 +0800

    project/style-xfce/style-theme/theme-pop-wm-numix

---
+++ project/style-xfce/style-theme/theme-pop-wm-numix/README.md
@@ -0,0 +1,57 @@
+
+# Prototype howto install pop-gtk-theme and numix-gtk-theme and numix-icon-theme-circle
+
+
+## Prepare
+
+* [prototype/de-basic/play-xfce](../../../de-basic/play-xfce)
+* [prototype/style-platformtheme/qt5-style-plugins](../../../style-platformtheme/qt5-style-plugins)
+
+
+## Package
+
+* [pop-gtk-theme](https://packages.ubuntu.com/focal/pop-gtk-theme)
+* [pop-icon-theme](https://packages.ubuntu.com/focal/pop-icon-theme)
+* [numix-gtk-theme](https://packages.ubuntu.com/focal/numix-gtk-theme)
+* [numix-icon-theme-circle](https://packages.ubuntu.com/focal/numix-icon-theme-circle)
+
+
+## Howto
+
+### install
+
+``` sh
+$ ./install.sh
+```
+
+or
+
+``` sh
+$ make install
+```
+
+
+### package-install
+
+``` sh
+$ ./package-install.sh
+```
+
+or
+
+``` sh
+$ make package-install
+```
+
+
+### config-install
+
+``` sh
+$ ./config-install.sh
+```
+
+or
+
+``` sh
+$ make config-install
+```
---
+++ project/style-xfce/style-theme/theme-pop-wm-numix/config-install.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+################################################################################
+### Head: theme
+##
+theme_config_install () {
+
+	theme_config_install_wm_theme
+	theme_config_install_ui_theme
+	theme_config_install_icon_theme
+
+}
+
+theme_config_install_wm_theme () {
+
+	## ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+	## <property name="theme" type="string" value="Numix"/>
+
+	echo "xfconf-query -c 'xfwm4' -p '/general/theme' -s 'Numix'"
+	xfconf-query -c 'xfwm4' -p '/general/theme' -s 'Numix'
+
+}
+
+theme_config_install_ui_theme () {
+
+	## ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
+	## <property name="ThemeName" type="string" value="Pop-dark"/>
+
+	echo "xfconf-query -c 'xsettings' -p '/Net/ThemeName' -s 'Pop-dark'"
+	xfconf-query -c 'xsettings' -p '/Net/ThemeName' -s 'Pop-dark'
+
+}
+
+theme_config_install_icon_theme () {
+
+	## ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
+	## <property name="IconThemeName" type="string" value="Numix-Circle"/>
+
+	echo "xfconf-query -c 'xsettings' -p '/Net/IconThemeName' -s 'Numix-Circle'"
+	xfconf-query -c 'xsettings' -p '/Net/IconThemeName' -s 'Numix-Circle'
+
+}
+
+theme_config_install_icon_theme_pop () {
+
+	## ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
+	## <property name="IconThemeName" type="string" value="Pop"/>
+
+	echo "xfconf-query -c 'xsettings' -p '/Net/IconThemeName' -s 'Pop'"
+	xfconf-query -c 'xsettings' -p '/Net/IconThemeName' -s 'Pop'
+
+}
+
+##
+### Tail: theme
+################################################################################
+
+
+################################################################################
+### Head: main
+##
+main_config_install () {
+
+	theme_config_install
+
+
+}
+## start
+main_config_install
+
+##
+### Tail: main
+################################################################################
