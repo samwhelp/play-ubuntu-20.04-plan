d2efdffba6a32528372612a2a9d09070d21e7b6d
samwhelp <samwhelp@users.noreply.github.com>
2020-04-27 19:44:45 +0800

    fix

--- prototype/xfce/config-install.sh
+++ prototype/xfce/config-install.sh
@@ -47,6 +47,75 @@ xfce4_config_install () {
 
 
 ################################################################################
+### Head: theme
+##
+theme_config_install () {
+
+	theme_config_install_wm_theme
+	theme_config_install_ui_theme
+	theme_config_install_icon_theme
+	theme_config_install_cursor_theme
+
+}
+
+theme_config_install_wm_theme () {
+
+	## ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+	## <property name="theme" type="string" value="NumixBlue"/>
+
+	echo "xfconf-query -c 'xfwm4' -p '/general/theme' -s 'NumixBlue'"
+	xfconf-query -c 'xfwm4' -p '/general/theme' -s 'NumixBlue'
+
+}
+
+theme_config_install_ui_theme () {
+
+	## ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
+	## <property name="ThemeName" type="string" value="NumixBlue"/>
+
+	echo "xfconf-query -c 'xsettings' -p '/Net/ThemeName' -s 'NumixBlue'"
+	xfconf-query -c 'xsettings' -p '/Net/ThemeName' -s 'NumixBlue'
+
+}
+
+theme_config_install_icon_theme () {
+
+	## ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
+	## <property name="IconThemeName" type="string" value="Numix"/>
+
+	echo "xfconf-query -c 'xsettings' -p '/Net/IconThemeName' -s 'Numix'"
+	xfconf-query -c 'xsettings' -p '/Net/IconThemeName' -s 'Numix'
+
+}
+
+theme_config_install_cursor_theme () {
+
+	## ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
+
+	## <property name="CursorThemeName" type="string" value="default"/>
+	## <property name="CursorThemeSize" type="int" value="24"/>
+
+	## <property name="CursorThemeName" type="string" value="breeze_cursors"/>
+	## <property name="CursorThemeSize" type="int" value="24"/>
+
+	## <property name="CursorThemeName" type="string" value="Breeze_Snow"/>
+	## <property name="CursorThemeSize" type="int" value="24"/>
+
+
+	echo "xfconf-query -c 'xsettings' -p '/Gtk/CursorThemeName' -s 'breeze_cursors'"
+	xfconf-query -c 'xsettings' -p '/Gtk/CursorThemeName' -s "breeze_cursors"
+
+	echo "xfconf-query -c 'xsettings' -p '/Gtk/CursorThemeSize' -s '24'"
+	xfconf-query -c 'xsettings' -p '/Gtk/CursorThemeSize' -s "24"
+
+}
+
+##
+### Tail: theme
+################################################################################
+
+
+################################################################################
 ### Head: pcmanfm-qt
 ##
 pcmanfm_qt_config_install () {
@@ -189,6 +258,8 @@ gtk2_config_install () {
 ##
 main_config_install () {
 	xfce4_config_install
+
+	#theme_config_install
 
 	pcmanfm_qt_config_install
 
