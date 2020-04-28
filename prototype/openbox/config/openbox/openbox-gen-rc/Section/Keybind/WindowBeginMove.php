c1a6ac513ef4c6cf663a655e4bcc47088a1ab248
samwhelp <samwhelp@users.noreply.github.com>
2020-04-29 00:36:58 +0800

    move and resize

--- prototype/openbox/config/openbox/openbox-gen-rc/Section/Keybind/WindowBeginMove.php
+++ prototype/openbox/config/openbox/openbox-gen-rc/Section/Keybind/WindowBeginMove.php
@@ -1,8 +1,85 @@
 
 
+	<!-- Window Begin Move //-->
 	<keybind key="W-e">
 		<action name="UnmaximizeFull"/>
 		<action name="Move"/>
+	</keybind>
+
+
+	<!-- Version: [k j h l] //-->
+	<!-- Window Move To North (Top) //-->
+	<keybind key="W-S-k">
+		<action name="UnmaximizeFull"/>
+		<action name="MoveRelative">
+			<x>0</x>
+			<y>-10</y>
+		</action>
+	</keybind>
+	
+	<!-- Window Move To South (Bottom) //-->
+	<keybind key="W-S-j">
+		<action name="UnmaximizeFull"/>
+		<action name="MoveRelative">
+			<x>0</x>
+			<y>+10</y>
+		</action>
+	</keybind>
+
+	<!-- Window Move To West (Left) //-->
+	<keybind key="W-S-h">
+		<action name="UnmaximizeFull"/>
+		<action name="MoveRelative">
+			<x>-10</x>
+			<y>0</y>
+		</action>
+	</keybind>
+
+	<!-- Window Move To East (Right) //-->
+	<keybind key="W-S-l">
+		<action name="UnmaximizeFull"/>
+		<action name="MoveRelative">
+			<x>+10</x>
+			<y>0</y>
+		</action>
+	</keybind>
+
+
+	<!-- Version: [Up Down Left Right] //-->
+	<!-- Window Move To North (Top) //-->
+	<keybind key="W-S-Up">
+		<action name="UnmaximizeFull"/>
+		<action name="MoveRelative">
+			<x>0</x>
+			<y>-10</y>
+		</action>
+	</keybind>
+	
+	<!-- Window Move To South (Bottom) //-->
+	<keybind key="W-S-Down">
+		<action name="UnmaximizeFull"/>
+		<action name="MoveRelative">
+			<x>0</x>
+			<y>+10</y>
+		</action>
+	</keybind>
+
+	<!-- Window Move To West (Left) //-->
+	<keybind key="W-S-Left">
+		<action name="UnmaximizeFull"/>
+		<action name="MoveRelative">
+			<x>-10</x>
+			<y>0</y>
+		</action>
+	</keybind>
+
+	<!-- Window Move To East (Right) //-->
+	<keybind key="W-S-Right">
+		<action name="UnmaximizeFull"/>
+		<action name="MoveRelative">
+			<x>+10</x>
+			<y>0</y>
+		</action>
 	</keybind>
 
 <?php
@@ -11,6 +88,7 @@
 ## Help
 
 * http://openbox.org/wiki/Help:Actions#Move
+* http://openbox.org/wiki/Help:Actions#MoveRelative
 
 */
 ?>
