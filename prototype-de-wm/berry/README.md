c2bef781496f1225a41c8ec528c28f1931d2c209
samwhelp <samwhelp@users.noreply.github.com>
2020-05-03 17:29:53 +0800

    prototype-de-wm/berry

---
+++ prototype-de-wm/berry/README.md
@@ -0,0 +1,82 @@
+
+# Prototype berry
+
+
+## Spec
+
+* [keybind](spec-keybind.md)
+* [mousebind](spec-mousebind.md)
+
+## Note
+
+* note-ubuntu-20.04 / [berry](https://samwhelp.github.io/note-ubuntu-20.04/read/subject/berry/)
+
+
+## Tool
+
+* [run-bspwm-on-xephyr](../../tool/xephyr/develop-tool/run-berry-on-xephyr/)
+
+
+## Link
+
+* https://berrywm.org/installation.html
+* https://github.com/JLErvin/berry
+
+
+## Config File Path
+
+* [~/.config/berry/berryrc](config/berry/default/berryrc)
+* [~/.config/berry/sxhkdrc](config/berry/default/sxhkdrc)
+
+
+## Howto
+
+### build-install
+
+``` sh
+$ ./build-install.sh
+```
+
+or
+
+``` sh
+$ make build-install
+```
+
+### install
+
+``` sh
+$ ./install.sh
+```
+
+or
+
+``` sh
+$ make install
+```
+
+
+### package-install
+
+``` sh
+$ ./package-install.sh
+```
+
+or
+
+``` sh
+$ make package-install
+```
+
+
+### config-install
+
+``` sh
+$ ./config-install.sh
+```
+
+or
+
+``` sh
+$ make config-install
+```
---
+++ prototype-de-wm/berry/build-install.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+
+##set -e
+
+
+################################################################################
+### Head: berry
+##
+berry_build_install () {
+
+	berry_build_install_prepare_dir
+
+	berry_build_install_package_build_essential
+	
+	berry_build_install_clone
+	
+	berry_build_install_build
+	
+	berry_build_install_put_xsession
+
+}
+
+berry_build_install_prepare_dir () {
+
+	mkdir -p var
+
+}
+
+berry_build_install_package_build_essential () {
+
+	##
+	## https://berrywm.org/installation.html
+	## https://github.com/JLErvin/berry
+	##
+
+local package_list='
+	build-essential
+	libx11-dev
+	libxft-dev
+	libxinerama-dev
+'
+
+	sudo apt-get install $package_list
+
+}
+
+
+berry_build_install_clone () {
+
+	
+	git clone https://github.com/JLErvin/berry.git var/berry
+
+
+}
+
+berry_build_install_build () {
+
+	
+	cd var/berry
+	
+	make
+	
+	sudo make install
+
+
+}
+
+##
+### Tail: berry
+################################################################################
+
+
+################################################################################
+### Head: main
+##
+main_build_install () {
+
+	berry_build_install
+
+}
+
+main_build_install
+##
+### Tail: main
+################################################################################
