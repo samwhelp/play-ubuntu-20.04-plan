82c488d0c4f16b067db1ca439f457128be6b137b
samwhelp <samwhelp@users.noreply.github.com>
2020-05-05 14:29:47 +0800

    update-alternatives --set editor

---
+++ tool/vim/config-editor.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+## update-alternatives --display editor
+
+## update-alternatives --list editor
+sudo update-alternatives --set editor /usr/bin/vim.basic
+
+
+## or interactive
+## sudo update-alternatives --config editor
+
+
+## or gui
+## galternatives
