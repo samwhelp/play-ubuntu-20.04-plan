a3f6de1b774280688ddceefb08b63047704c0a2a
samwhelp <samwhelp@users.noreply.github.com>
2020-05-07 02:49:21 +0800

    fix

--- prototype/awesome/config/awesome/start/style/experiment/rc.lua
+++ prototype/awesome/config/awesome/start/style/experiment/rc.lua
@@ -212,6 +212,7 @@ tag.connect_signal('request::default_layouts', function()
 	awful.layout.append_default_layouts({
 		awful.layout.suit.tile,
 		awful.layout.suit.tile.bottom,
+		awful.layout.suit.magnifier,
 		awful.layout.suit.floating,
 		awful.layout.suit.max,
 		-- awful.layout.suit.tile.left,
@@ -220,8 +221,7 @@ tag.connect_signal('request::default_layouts', function()
 		--awful.layout.suit.fair.horizontal,
 		--awful.layout.suit.spiral,
 		-- awful.layout.suit.spiral.dwindle,
-		-- awful.layout.suit.max.fullscreen,
-		-- awful.layout.suit.magnifier,
+		-- -- awful.layout.suit.max.fullscreen,
 		-- awful.layout.suit.corner.nw,
 	})
 
