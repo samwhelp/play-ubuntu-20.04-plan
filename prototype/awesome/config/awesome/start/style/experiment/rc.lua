977f69b31f69b1602947ee127fc44ad17e78cf2d
samwhelp <samwhelp@users.noreply.github.com>
2020-05-08 05:38:07 +0800

    fix

--- prototype/awesome/config/awesome/start/style/experiment/rc.lua
+++ prototype/awesome/config/awesome/start/style/experiment/rc.lua
@@ -121,11 +121,14 @@ print('gears.filesystem.get_configuration_dir() = ' .. gears.filesystem.get_conf
 
 beautiful.init(gears.filesystem.get_configuration_dir() .. 'theme/experiment/theme.lua')
 
--- Themes define colours, icons, font and wallpapers.
+-- ## Themes define colours, icons, font and wallpapers.
 --beautiful.init(gears.filesystem.get_themes_dir() .. 'default/theme.lua')
 --beautiful.init(gears.filesystem.get_themes_dir() .. 'gtk/theme.lua')
+--beautiful.init(gears.filesystem.get_themes_dir() .. 'zenburn/theme.lua')
+--beautiful.init(gears.filesystem.get_themes_dir() .. 'xresources/theme.lua')
+--beautiful.init(gears.filesystem.get_themes_dir() .. 'sky/theme.lua')
 
--- Wallpaper
+-- ## Wallpaper
 --beautiful.wallpaper = '/usr/share/backgrounds/Manhattan_Sunset_by_Giacomo_Ferroni.jpg'
 --beautiful.wallpaper = '/usr/share/backgrounds/Spices_in_Athens_by_Makis_Chourdakis.jpg'
 beautiful.wallpaper = '/usr/share/backgrounds/Frozen_sunset_on_the_lake_by_Manuel_Arslanyan.jpg'
